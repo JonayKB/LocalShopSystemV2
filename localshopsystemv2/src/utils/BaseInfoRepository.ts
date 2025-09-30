9f5f1e44aedc9973e022f97981dfd14c5804b188
Jonay Contreras <88539815+JonayKB@users.noreply.github.com>
2025-09-30 21:09:19 +0100

    Update BASE_URL for development and production

--- localshopsystemv2/src/utils/BaseInfoRepository.ts
+++ localshopsystemv2/src/utils/BaseInfoRepository.ts
@@ -1,8 +1,8 @@
 class BaseInfoRepositoryDevelopment {
-  public static readonly BASE_URL = 'http://localhost:8080/kiosco/';
+  public static readonly BASE_URL = 'http://localhost:56784/kiosco/';
 }
 class BaseInfoRepositoryProduction {
-  public static readonly BASE_URL = 'http://kioscobotanico.es:8080/kiosco/';
+  public static readonly BASE_URL = 'http://kioscobotanico.es:56784/kiosco/';
 
 }
 export const BaseInfoRepository = process.env.NODE_ENV === 'development' ? BaseInfoRepositoryDevelopment : BaseInfoRepositoryProduction;
